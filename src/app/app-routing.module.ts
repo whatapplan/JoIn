3948a64877fc3e661ea3b42fdc0d54486fe4b98d
mariodemartinis5 <mariodemartinis5@gmail.com>
2021-10-13 10:10:48 +0200

    Module Register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,11 @@ const routes: Routes = [
       import('./modules/plan-creation/plan-creation.module').then(
         (m) => m.PlanCreationPageModule
       ),
+  },  {
+    path: 'register',
+    loadChildren: () => import('./modules/register/register.module').then( m => m.RegisterPageModule)
   }
+
 ];
 
 @NgModule({
