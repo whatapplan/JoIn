9bfa296cb376eea0b098bd37ca9d121e453aaa06
mariodemartinis5 <mariodemartinis5@gmail.com>
2021-10-13 10:16:52 +0200

    Module Register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },  {
+    path: 'register',
+    loadChildren: () => import('./modules/register/register.module').then( m => m.RegisterPageModule)
   },
+
   
 
 ];
