5185503fc24c1eec82271627c6d4e5cde1e88158
mariodemartinis5 <mariodemartinis5@gmail.com>
2021-10-30 16:44:40 +0200

    side menu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor( private menuCtrl: MenuController) { }
 
   ngOnInit() {
   }
-
+ 
+  Close(){
+    this.menuCtrl.toggle();
+  }
 }
