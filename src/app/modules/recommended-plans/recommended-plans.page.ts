1f70de619c3530870f175a62c9f81d7e87cfb458
mariodemartinis5 <mariodemartinis5@gmail.com>
2021-11-06 13:37:11 +0100

    swipe error + plan centrado

--- src/app/modules/recommended-plans/recommended-plans.page.ts
+++ src/app/modules/recommended-plans/recommended-plans.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Gesture, GestureController, IonCard, Platform } from '@ionic/angular';
 import {Plan} from '../../core/models/plan';
 import { CrudService } from '../../core/services/http/crud-service.service';
 
@@ -7,11 +8,13 @@ import { CrudService } from '../../core/services/http/crud-service.service';
   templateUrl: './recommended-plans.page.html',
   styleUrls: ['./recommended-plans.page.scss'],
 })
-export class RecommendedPlansPage implements OnInit {
+export class RecommendedPlansPage implements AfterViewInit {
   recommendedPlans : Plan[] = [];
   plans : Plan[] = [];
-  constructor(private crudService : CrudService ) { 
-   
+
+  @ViewChildren(IonCard, {read: ElementRef}) cards: QueryList<ElementRef>;
+
+  constructor(private crudService : CrudService, private gestureCtrl : GestureController, private platform: Platform) { 
     this.crudService.getPlans().subscribe((res) => {res.map((t)=>{
       let plan = {
         id: t.payload.doc.id,
@@ -20,118 +23,77 @@ export class RecommendedPlansPage implements OnInit {
       let pplan = t.payload.doc.data() as Plan;
       this.plans.push(plan);
       })});
+       // this.filterPlansByCategory();
+      this.recommendedPlans=this.plans;  // esto tengo que cambiarlo en un futuro
+  }
+  async ngAfterViewInit(){
 
-      // this.filterPlansByCategory();
-      this.recommendedPlans=this.plans; // esto tengo que cambiarlo en un futuro
+   const cardArray = this.cards.toArray();
+   console.log(cardArray.length);
+   this.useSwipe(cardArray);
+  }
+  useSwipe(cardArray) {
+    for(let i=0; i<cardArray.length; i++){
+      const card = cardArray[i];
+      console.log(card);
+      const gesture: Gesture = this.gestureCtrl.create({
+        el: card.nativeElement,
+        gestureName: 'swipe',
+        onStart: ev => {
+
+        },
+        onMove: ev => {
+          console.log('ev: ', ev);
+          card.nativeElement.style.transform = `translateX(${ev.deltaX}px)`;
+          this.setCardColor(ev.deltaX, card.nativeElement);
+        },
+        onEnd: ev => {
+          if(ev.deltaX > 150 ){
+            card.nativeElement.style.height = this.platform.height() + 'px';
+            card.nativeElement.style.transform =  `translateX(${+ this.platform.width() * 2}px) `;
+           
+          }else if(ev.deltaX < -150){ 
+            card.nativeElement.style.transform =  `translateX(${- this.platform.width() * 2}px) `;
+
+          }else{
+            card.nativeElement.style.transform = '';
+          }
+        }
+      },true);
+      gesture.enable(true);
+    }
   }
 
-  ngOnInit() {
-    
-  }
+ 
+
   filters : string[]=["Cervezas","Pasear al perro","Béisbol",
   "Baloncesto","Fútbol","Tenis","Golf","Surf","Arte y exposiciones",
   "Cine","Teatro","Jam Session","Música en directo","Clubbing","Explora la ciudad"];
   
-  
-  // slideOpts = {
-  //   initialSlide: 1,
-  //   speed: 400,
-  // };
+  setCardColor(x, element){
+    let color = '';
+    const abs = Math.abs(x);
+    const min = Math.trunc(Math.min(16*16-abs, 16*16));
+    const hexCode = this.decimalToHex(min,2);
 
-
-
-
-
-
-
-
-
-  slideOpts = {
-    speed:700,
-    on: {
-      beforeInit() {
-        const swiper = this;
-        swiper.classNames.push(`${swiper.params.containerModifierClass}flip`);
-        swiper.classNames.push(`${swiper.params.containerModifierClass}3d`);
-        const overwriteParams = {
-          slidesPerView: 1,
-          slidesPerColumn: 1,
-          slidesPerGroup: 1,
-          watchSlidesProgress: true,
-          spaceBetween: 0,
-          virtualTranslate: true,
-        };
-        swiper.params = Object.assign(swiper.params, overwriteParams);
-        swiper.originalParams = Object.assign(swiper.originalParams, overwriteParams);
-      },
-      setTranslate() {
-        const swiper = this;
-        const { $, slides, rtlTranslate: rtl } = swiper;
-        for (let i = 0; i < slides.length; i += 1) {
-          const $slideEl = slides.eq(i);
-          let progress = $slideEl[0].progress;
-          if (swiper.params.flipEffect.limitRotation) {
-            progress = Math.max(Math.min($slideEl[0].progress, 1), -1);
-          }
-          const offset$$1 = $slideEl[0].swiperSlideOffset;
-          const rotate = -180 * progress;
-          let rotateY = rotate;
-          let rotateX = 0;
-          let tx = -offset$$1;
-          let ty = 0;
-          if (!swiper.isHorizontal()) {
-            ty = tx;
-            tx = 0;
-            rotateX = -rotateY;
-            rotateY = 0;
-          } else if (rtl) {
-            rotateY = -rotateY;
-          }
-  
-           $slideEl[0].style.zIndex = -Math.abs(Math.round(progress)) + slides.length;
-  
-           if (swiper.params.flipEffect.slideShadows) {
-            // Set shadows
-            let shadowBefore = swiper.isHorizontal() ? $slideEl.find('.swiper-slide-shadow-left') : $slideEl.find('.swiper-slide-shadow-top');
-            let shadowAfter = swiper.isHorizontal() ? $slideEl.find('.swiper-slide-shadow-right') : $slideEl.find('.swiper-slide-shadow-bottom');
-            if (shadowBefore.length === 0) {
-              shadowBefore = swiper.$(`<div class="swiper-slide-shadow-${swiper.isHorizontal() ? 'left' : 'top'}"></div>`);
-              $slideEl.append(shadowBefore);
-            }
-            if (shadowAfter.length === 0) {
-              shadowAfter = swiper.$(`<div class="swiper-slide-shadow-${swiper.isHorizontal() ? 'right' : 'bottom'}"></div>`);
-              $slideEl.append(shadowAfter);
-            }
-            if (shadowBefore.length) shadowBefore[0].style.opacity = Math.max(-progress, 0);
-            if (shadowAfter.length) shadowAfter[0].style.opacity = Math.max(progress, 0);
-          }
-          $slideEl
-            .transform(`translate3d(${tx}px, ${ty}px, 0px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`);
-        }
-      },
-      setTransition(duration) {
-        const swiper = this;
-        const { slides, activeIndex, $wrapperEl } = swiper;
-        slides
-          .transition(duration)
-          .find('.swiper-slide-shadow-top, .swiper-slide-shadow-right, .swiper-slide-shadow-bottom, .swiper-slide-shadow-left')
-          .transition(duration);
-        if (swiper.params.virtualTranslate && duration !== 0) {
-          let eventTriggered = false;
-          // eslint-disable-next-line
-          slides.eq(activeIndex).transitionEnd(function onTransitionEnd() {
-            if (eventTriggered) return;
-            if (!swiper || swiper.destroyed) return;
-  
-            eventTriggered = true;
-            swiper.animating = false;
-            const triggerEvents = ['webkitTransitionEnd', 'transitionend'];
-            for (let i = 0; i < triggerEvents.length; i += 1) {
-              $wrapperEl.trigger(triggerEvents[i]);
-            }
-          });
-        }
-      }
+    if (x < 0){
+      color = '#FF' + hexCode + hexCode;
+    }else{
+      color = '#' + hexCode + 'FF' + hexCode;
     }
-  };
+    element.style.backgroundColor = color;
+
+  }
+
+  decimalToHex(d, padding) {
+    let hex = Number(d).toString(16);
+    padding = typeof padding === 'undefined' || padding === null ? (padding = 2) : padding;
+
+    while (hex.length < padding) {
+      hex = '0' + hex;
+    }
+    return hex;
+  }
+  
+  
 }
