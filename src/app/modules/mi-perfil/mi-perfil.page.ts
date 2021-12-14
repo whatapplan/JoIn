e61babbf82bd4756ee14a90a6c805fd5b40f4e1b
lilnele <91592056+lilnele@users.noreply.github.com>
2021-12-14 15:18:11 +0100

    fehca

--- src/app/modules/mi-perfil/mi-perfil.page.ts
+++ src/app/modules/mi-perfil/mi-perfil.page.ts
@@ -25,6 +25,9 @@ export class MiPerfilPage implements OnInit {
   numplans:number;
   edadperf:number;
   dateBirth : any;
+  userFrom: any;
+  año: number;
+  usuariaDesde: String;
   constructor(private auth: AuthService,
     private crud: CrudService,
     private alertController:AlertController,
@@ -34,9 +37,14 @@ export class MiPerfilPage implements OnInit {
     private ui : UiHelper) { 
       this.user = this.auth.loggedUser;
       let time= this.user.dateBirth as unknown as Timestamp;
+     let tiempoDesde = this.user.userFrom as unknown as Timestamp;
+    this.userFrom = new Date(tiempoDesde.seconds*1000);
+    
       this.dateBirth = new Date(time.seconds*1000);
       this.edadperf = this.sacaredad();
       console.log(this.edadperf);
+      console.log(this.userFrom.getFullYear());
+      
   }
 
   ngOnInit() {
@@ -47,6 +55,8 @@ export class MiPerfilPage implements OnInit {
   this.numplans = this.user.createdPlans.length;
   // this.edadperf = 27;
   this.imagen = this.user.image;
+ this.usuariaDesde = "Usuaria desde" + " " + this.sacarMes() + " "+ "de" + " " + this.userFrom.getFullYear();
+  
   }
 
   sacaredad(){
@@ -54,9 +64,45 @@ export class MiPerfilPage implements OnInit {
         return Math.ceil((timeDiff / (1000 * 3600 * 24)) / 365);
     
   }
-  usuarioDesde(){
+  sacarMes(){
+    if(this.userFrom.getMonth()==1){
+      return "Enero"
+    }
+    if(this.userFrom.getMonth()==2){
+      return "Febrero"
+    }
+    if(this.userFrom.getMonth()==3){
+      return "Marzo"
+    }
+    if(this.userFrom.getMonth()==4){
+      return "Abril"
+    }
+    if(this.userFrom.getMonth()==5){
+      return "Mayo"
+    }
+    if(this.userFrom.getMonth()==6){
+      return "Junio"
+    }
+    if(this.userFrom.getMonth()==7){
+      return "Julio"
+    }
+    if(this.userFrom.getMonth()==8){
+      return "Agosto"
+    }
+    if(this.userFrom.getMonth()==9){
+      return "Septiembre"
+    }
+    if(this.userFrom.getMonth()==10){
+      return "Octubre"
+    }
+    if(this.userFrom.getMonth()==11){
+      return "Noviembre"
+    }
+    if(this.userFrom.getMonth()==12){
+      return "Diciembre"
+    }
 
-  let tiempo = this.user.userFrom;
+
  
 
   }
